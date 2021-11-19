9ef023996dbf5bf62cb4fbc2d2447a1a254de1e5
iSemicolon <palash.samanta7497@gmail.com>
2021-11-20 00:09:50 +0530

    Right Pyramid  Pattern

--- src/PATTERNS/StarPatterns/RightPyramid/RightPyramid.java
+++ src/PATTERNS/StarPatterns/RightPyramid/RightPyramid.java
@@ -1,10 +1,34 @@
 package PATTERNS.StarPatterns.RightPyramid;
 
+/*
+ 
+*
+**
+***
+****
+*****
 
+ 
+ */
 
 public class RightPyramid {
 
 	public static void main(String[] args) {
+		
+		for(int row=1;row<=5;row++)
+		{ 
+			
+			for(int col=1;col<=row;col++)
+			{  
+				
+				System.out.print("*");
+				
+			
+			}
+			
+			System.out.println();
+		}
+	}
 	
 
 }
