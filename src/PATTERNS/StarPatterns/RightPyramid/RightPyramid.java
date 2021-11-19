4485cedcc234df26e5b6c63dc0f2dc46080644ae
iSemicolon <palash.samanta7497@gmail.com>
2021-11-20 00:06:46 +0530

    Reverse Right Pyramid  Pattern

--- src/PATTERNS/StarPatterns/RightPyramid/RightPyramid.java
+++ src/PATTERNS/StarPatterns/RightPyramid/RightPyramid.java
@@ -1,32 +1,10 @@
 package PATTERNS.StarPatterns.RightPyramid;
 
-/*
- 
-*****
-****
-***
-**
-*
 
- */
 
 public class RightPyramid {
 
 	public static void main(String[] args) {
-		
-		for(int row=1;row<=5;row++)
-		{ 
-			
-			for(int col=5;col>=row;col--)
-			{  
-				
-				System.out.print("*");
-				
-			
-			}
-			
-			System.out.println();
-		}
-	}
+	
 
 }
---
+++ src/PATTERNS/StarPatterns/RightReversePyramid/ReverseRightPyramid.java
@@ -0,0 +1,32 @@
+package PATTERNS.StarPatterns.RightReversePyramid;
+
+/*
+
+*****
+****
+***
+**
+*
+
+ */
+public class ReverseRightPyramid {
+
+	public static void main(String[] args) {
+		
+		for(int row=1;row<=5;row++)
+		{ 
+			
+			for(int col=5;col>=row;col--)
+			{  
+				
+				System.out.print("*");
+				
+			
+			}
+			
+			System.out.println();
+		}
+	
+	}
+
+}
