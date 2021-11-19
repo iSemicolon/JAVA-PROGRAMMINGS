02f33e1223df64dd6024393629670e2b6a04be53
iSemicolon <palash.samanta7497@gmail.com>
2021-11-20 00:21:40 +0530

    Left Pyramid  Pattern

--- src/PATTERNS/StarPatterns/LeftPyramid/LeftPyramid.java
+++ src/PATTERNS/StarPatterns/LeftPyramid/LeftPyramid.java
@@ -1,10 +1,32 @@
 package PATTERNS.StarPatterns.LeftPyramid;
 
+/*
+ 
+ *****
+ ****
+ ***
+ **
+ *
+ 
+ */
 public class LeftPyramid {
 
 	public static void main(String[] args) {
 
-		
+		for(int row=1;row<=5;row++)
+		{ 
+			
+			for(int col=5;col>=row;col--)
+			{  
+				
+				System.out.print("*");
+				
+			
+			}
+			
+			System.out.println();
+		}
+
 
 	}
 
