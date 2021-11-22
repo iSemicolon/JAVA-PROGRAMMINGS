47f8ee56529868cc73f46f4a44556df3d501fa08
iSemicolon <palash.samanta7497@gmail.com>
2021-11-22 14:15:22 +0530

    Average of Array

---
+++ src/ARRAY/CheckArrayElements/ArrayAverage.java
@@ -0,0 +1,36 @@
+package ARRAY.CheckArrayElements;
+
+import java.util.Scanner;
+
+public class ArrayAverage {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Enter Array Size");
+		Scanner sc=new Scanner(System.in);
+
+		int n=sc.nextInt();
+		double arr[]=new double[n];
+
+		double sum=0;
+
+		System.out.println("Enter array  elements");
+		for(int i=0;i<arr.length;i++)
+		{
+		arr[i]=sc.nextInt();
+
+		}
+
+		for(int i=0;i<arr.length;i++)
+		{
+
+		sum=sum+arr[i];
+		}
+		
+		double avg=sum/arr.length;
+
+		System.out.println("Average of Array Elements:"+avg);
+		}
+	
+
+}
