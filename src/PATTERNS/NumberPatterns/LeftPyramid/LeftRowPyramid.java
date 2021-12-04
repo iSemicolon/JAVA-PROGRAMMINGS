cdec9e94d48fc232166a9475635dfdeaad4e18c9
iSemicolon <palash.samanta7497@gmail.com>
2021-12-04 19:24:33 +0530

    Left Row Pyramid Number Pattern

---
+++ src/PATTERNS/NumberPatterns/LeftPyramid/LeftRowPyramid.java
@@ -0,0 +1,37 @@
+package PATTERNS.NumberPatterns.LeftPyramid;
+
+/*
+1 
+2 2 
+3 3 3  
+ */
+
+public class LeftRowPyramid {
+
+
+	public static void main(String[] args) {
+
+		for(int row=1;row<=3;row++)
+		{ 
+			for(int col=3;col>=row;col--)
+			{  
+
+				System.out.print(" ");
+
+
+			}
+
+			for(int col=1;col<=row;col++)
+			{  
+
+				System.out.print(row+" ");
+
+			}
+
+			System.out.println();
+		}
+
+
+	}
+
+}
