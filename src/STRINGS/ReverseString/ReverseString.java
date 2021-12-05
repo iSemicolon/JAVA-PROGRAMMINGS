ab854aa92cc7efee46201af562e5c44500ef570b
iSemicolon <palash.samanta7497@gmail.com>
2021-12-05 15:18:39 +0530

    Reverse String Without Using Inbuilt Function

---
+++ src/STRINGS/ReverseString/ReverseString.java
@@ -0,0 +1,22 @@
+package STRINGS.ReverseString;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter String");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		//String s1=sc.nextLine();
+
+		char ch[]=s.toCharArray();
+
+		for(int i=ch.length-1;i>=0;i--)
+		{
+		System.out.print(ch[i]);
+		}
+	}
+
+}
