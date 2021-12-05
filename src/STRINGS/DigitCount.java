34ae47fd1c2046f8a5e00244747777fcd0c983fc
iSemicolon <palash.samanta7497@gmail.com>
2021-12-05 15:24:58 +0530

    Numerical Digit Count In String

---
+++ src/STRINGS/DigitCount.java
@@ -0,0 +1,31 @@
+package STRINGS;
+
+import java.util.Scanner;
+
+public class DigitCount {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter String");
+
+		Scanner sc=new Scanner(System.in);
+
+		String str=sc.nextLine();
+		int count=0;
+		for(int i=0; i<str.length();i++)
+		{
+			char a=str.charAt(i);
+
+			if(a>='0' && a<='9')
+			{
+				count++;
+			}
+		}
+		System.out.println("Numerical Digit Present: "+count);
+	}
+}
+
+
+
+
+
