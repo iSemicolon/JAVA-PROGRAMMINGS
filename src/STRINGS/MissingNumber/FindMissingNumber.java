91c28fd36a2fad5bbd1d6265e334b2325640c85f
iSemicolon <palash.samanta7497@gmail.com>
2023-03-02 19:17:43 +0530

    "Missing Number from duplicate Array"

--- src/STRINGS/MissingNumber/FindMissingNumber.java
+++ src/STRINGS/MissingNumber/FindMissingNumber.java
@@ -1,6 +1,7 @@
 package STRINGS.MissingNumber;
 
 
+import java.util.Arrays;
 
 public class FindMissingNumber {
 	
@@ -22,6 +23,42 @@ public class FindMissingNumber {
 		return expectedSum-sum;
 	}
 
+
+	public static  int missingNumberFromRepeated(int[] repeatTemp ){
+
+		//sort the array
+		Arrays.sort(repeatTemp);
+
+		for (int i=0; i<repeatTemp.length; i++){
+			System.out.println("hello: "+repeatTemp[i]);
+		}
+
+		//find repeated element testing only optional
+		int duplicateNumber=0;
+
+		for (int i=0; i<repeatTemp.length-1; i++){
+
+			if(repeatTemp[i]==repeatTemp[i+1]){
+				duplicateNumber=repeatTemp[i];
+				System.out.println("Duplicate Number: "+duplicateNumber);
+				break;
+			}
+
+		}
+
+		int missingNumber=0;
+		//starting from array first element
+		for (int i =1; i <= repeatTemp.length; i++) {
+			if (i != repeatTemp[i - 1]) {
+				missingNumber=i;
+				break;
+			}
+		}
+
+		return missingNumber;
+
+	}
+
 	public static void main(String args[])
 	{
 		System.out.println("Non-Repeated Array Operation");
@@ -29,6 +66,14 @@ public class FindMissingNumber {
 
 		int number=missingNumberFromNonRepetated(temp);
 		System.out.println("Missing Number is  :"+number);
+
+		System.out.println("Repeated Array Operation");
+		int repeatTemp[] = { 7, 3, 4, 5, 5, 6, 2 };
+
+		int missingNumber=missingNumberFromRepeated(repeatTemp);
+		System.out.println("Missing Numbers is: "+missingNumber);
+
+
 
 
 	}
